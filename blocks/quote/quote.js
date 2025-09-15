9d745648e427ca071b66b5d72edd52a98040c2f4
Diego Martins <diego.martins@vilt-group.com>
2025-09-15 10:09:59 -0300

    button field on quote block

---
+++ blocks/quote/quote.js
@@ -0,0 +1,8 @@
+/* necessário definir os estilos ao injetar as classes no block via JS */
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
+
+  const blockquote = document.createElement("blockquote");
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
+}
