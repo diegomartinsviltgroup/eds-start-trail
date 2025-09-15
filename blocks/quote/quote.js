1a30feaece06fd6ca6e3da93a6038e6fe9eab3dc
Diego Martins <diego.martins@vilt-group.com>
2025-09-15 11:01:14 -0300

    remove quote block

--- blocks/quote/quote.js
+++
@@ -1,8 +0,0 @@
-/* necessário definir os estilos ao injetar as classes no block via JS */
-export default function decorate(block) {
-  const [quoteWrapper] = block.children;
-
-  const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
-}
