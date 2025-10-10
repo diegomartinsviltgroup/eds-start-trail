cca68f856765fd0a2f748a698ccb98357e73252c
Diego Martins <diego.martins@vilt-group.com>
2025-10-10 13:53:17 -0300

    featured js config

--- blocks/About/about.js
+++ blocks/About/about.js
@@ -6,20 +6,4 @@ export default function decorate(block) {
   title.classList.add("about__title--wrapper");
   text.classList.add("about__text--wrapper");
   cta.classList.add("about__cta--wrapper");
-
-  // const container = document.createElement("div");
-  // container.classList.add("hero-carousel");
-
-  // items.forEach((element) => {
-  //   const [title, imageX, button] = element.children;
-
-  //   element.classList.add("hero-slide");
-  //   title.classList.add("hero-title");
-  //   imageX.classList.add("hero-image-x");
-  //   button.classList.add("hero-button");
-
-  //   container.appendChild(element);
-  // });
-
-  // block.appendChild(container);
 }
