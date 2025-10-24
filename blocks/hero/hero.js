788897206010be808db107d9f9c595c5890aef5d
Diego Martins <diego.martins@vilt-group.com>
2025-10-24 14:16:42 -0300

    hero design

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,26 +2,26 @@ export default function decorate(block) {
   const items = Array.from(block.children);
   const container = document.createElement("div");
   container.classList.add("hero-carousel");
-
   items.forEach((element) => {
-    const [title, imageX, button] = element.children;
+    const [title, text, imageX, button] = element.children;
+    console.log("0", element.children);
 
-    element.classList.add("hero-slide");
-    title.classList.add("hero-title");
-    imageX.classList.add("hero-image-x");
-    button.classList.add("hero-button");
-    const p = document.querySelector(".hero-title");
-    const h1 = document.createElement("h1");
-    title.appendChild(h1);
-    h1.textContent = p.textContent;
-    p.querySelector("p").remove();
-    const contentContainer = document.createElement("div");
-    contentContainer.classList.add("hero-content-container");
-    contentContainer.appendChild(title);
-    contentContainer.appendChild(button);
-    element.appendChild(contentContainer);
-    container.appendChild(element);
+    // element.classList.add("hero-slide");
+    // title.classList.add("hero-title");
+    // imageX.classList.add("hero-image-x");
+    // button.classList.add("hero-button");
+    // const p = document.querySelector(".hero-title");
+    // const h1 = document.createElement("h1");
+    // title.appendChild(h1);
+    // h1.textContent = p.textContent;
+    // // p.querySelector("p").remove();
+    // const contentContainer = document.createElement("div");
+    // contentContainer.classList.add("hero-content-container");
+    // contentContainer.appendChild(title);
+    // contentContainer.appendChild(button);
+    // element.appendChild(contentContainer);
+    // container.appendChild(element);
   });
 
-  block.appendChild(container);
+  // block.appendChild(container);
 }
