3a79efd381bede2b7b008869911d9cd979a5eda2
Diego Martins <diego.martins@vilt-group.com>
2025-10-29 13:27:53 -0300

    format

--- blocks/Events/events.js
+++ blocks/Events/events.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
   const items = Array.from(block.children);
   items.forEach((item) => {
-    item.classList.add("events-items-wrapper");
+    item.classList.add('events-items-wrapper');
     const [image, title, text, cta] = item.children;
-    image?.classList.add("events-item-image");
-    title?.classList.add("events-item-title");
-    text?.classList.add("events-item-text");
-    cta?.classList.add("events-item-cta");
+    image?.classList.add('events-item-image');
+    title?.classList.add('events-item-title');
+    text?.classList.add('events-item-text');
+    cta?.classList.add('events-item-cta');
   });
 }
