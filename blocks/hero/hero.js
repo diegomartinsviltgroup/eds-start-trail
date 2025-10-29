5e17f81828912ce31071de01b4014212b3dffaca
Diego Martins <diego.martins@vilt-group.com>
2025-10-29 13:12:29 -0300

    events carrossel

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,22 +1,21 @@
 export default function decorate(block) {
   const items = Array.from(block.children);
-  const container = document.createElement("div");
-  container.classList.add("hero-carousel");
+  const container = document.createElement('div');
+  container.classList.add('hero-carousel');
   items.forEach((element) => {
     const [title, imageX, text, button] = element.children;
-    console.log(title, text, imageX, button);
-    text.classList.add("hero-text");
-    element.classList.add("hero-slide");
-    title.classList.add("hero-title");
-    imageX.classList.add("hero-image-x");
-    button.classList.add("hero-button");
-    const p = document.querySelector(".hero-title");
-    const h1 = document.createElement("h1");
+    text.classList.add('hero-text');
+    element.classList.add('hero-slide');
+    title.classList.add('hero-title');
+    imageX.classList.add('hero-image-x');
+    button.classList.add('hero-button');
+    const p = document.querySelector('.hero-title');
+    const h1 = document.createElement('h1');
     title.appendChild(h1);
     h1.textContent = p.textContent;
-    p.querySelector("p").remove();
-    const contentContainer = document.createElement("div");
-    contentContainer.classList.add("hero-content-container");
+    p.querySelector('p').remove();
+    const contentContainer = document.createElement('div');
+    contentContainer.classList.add('hero-content-container');
     contentContainer.appendChild(title);
     contentContainer.appendChild(text);
     contentContainer.appendChild(button);
