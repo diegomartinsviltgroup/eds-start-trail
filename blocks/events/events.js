434cf39ef5348c68262f8d50fec55766ca806723
Diego Martins <diego.martins@vilt-group.com>
2025-10-31 17:07:20 -0300

    events carousel finished

--- blocks/events/events.js
+++ blocks/events/events.js
@@ -2,11 +2,24 @@ export default function decorate(block) {
   const items = Array.from(block.children);
 
   items.forEach((item) => {
-    item.classList.add('events-items-wrapper');
     const [image, title, text, cta] = item.children;
+    item.classList.add('events-items-wrapper');
     image?.classList.add('events-item-image');
     title?.classList.add('events-item-title');
     text?.classList.add('events-item-text');
-    cta?.classList.add('events-item-cta');
+    const innerCta = cta.querySelector('p');
+    const isEvent = innerCta.classList.contains('button-container');
+    if (isEvent) {
+      cta?.classList.add('events-item-cta');
+    } else {
+      cta.remove();
+      text?.classList.remove('events-item-text');
+      text?.classList.add('events-item-cta-exclusive');
+      image?.classList.remove('events-item-image');
+      image?.classList.add('events-item-title');
+      title?.classList.remove('events-item-title');
+      title?.classList.add('events-item-text');
+      item.classList.add('cta');
+    }
   });
 }
