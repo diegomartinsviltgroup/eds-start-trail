d6ae3d5109c78812d2645deeb30ef06236d2c80b
Diego Martins <diego.martins@vilt-group.com>
2025-11-03 11:11:12 -0300

    title json

--- blocks/events/events.js
+++ blocks/events/events.js
@@ -2,23 +2,23 @@ export default function decorate(block) {
   const items = Array.from(block.children);
   items.forEach((item) => {
     const [image, title, text, cta] = item.children;
-    item.classList.add("events-items-wrapper");
-    image?.classList.add("events-item-image");
-    title?.classList.add("events-item-title");
-    text?.classList.add("events-item-text");
-    const innerCta = cta.querySelector("p");
-    const isEvent = innerCta.classList.contains("button-container");
+    item.classList.add('events-items-wrapper');
+    image?.classList.add('events-item-image');
+    title?.classList.add('events-item-title');
+    text?.classList.add('events-item-text');
+    const innerCta = cta.querySelector('p');
+    const isEvent = innerCta.classList.contains('button-container');
     if (isEvent) {
-      cta?.classList.add("events-item-cta");
+      cta?.classList.add('events-item-cta');
     } else {
       cta.remove();
-      text?.classList.remove("events-item-text");
-      text?.classList.add("events-item-cta-exclusive");
-      image?.classList.remove("events-item-image");
-      image?.classList.add("events-item-title");
-      title?.classList.remove("events-item-title");
-      title?.classList.add("events-item-text");
-      item.classList.add("cta");
+      text?.classList.remove('events-item-text');
+      text?.classList.add('events-item-cta-exclusive');
+      image?.classList.remove('events-item-image');
+      image?.classList.add('events-item-title');
+      title?.classList.remove('events-item-title');
+      title?.classList.add('events-item-text');
+      item.classList.add('cta');
     }
   });
 }
