9668c062789ea0371f0e8931136dab5cdedaecea
Diego Martins <diego.martins@vilt-group.com>
2025-11-04 09:13:27 -0300

    events date

--- blocks/section-title/section-title.js
+++ blocks/section-title/section-title.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const [titleDiv] = block.children;
-  titleDiv.classList.add("section-title-wrapper");
+  titleDiv.classList.add('section-title-wrapper');
   const [title] = titleDiv.children;
-  title.classList.add("section-title-text");
+  title.classList.add('section-title-text');
 }
