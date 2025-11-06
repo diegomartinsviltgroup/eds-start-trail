e437aa3d5224843a3d317f8cd4cbad7ca377c2fe
Diego Martins <diego.martins@vilt-group.com>
2025-11-06 13:58:28 -0300

    about

--- blocks/About/about.js
+++ blocks/About/about.js
@@ -1,9 +1,8 @@
 export default function decorate(block) {
   const items = Array.from(block.children);
-  const [imageDiv, title, text, cta] = items;
+  const [imageDiv, text, cta] = items;
 
-  imageDiv.classList.add('about-image-wrapper');
-  title.classList.add('about-title-wrapper');
-  text.classList.add('about-text-wrapper');
-  cta.classList.add('about-cta-wrapper');
+  imageDiv.classList.add("about-image-wrapper");
+  text.classList.add("about-text-wrapper");
+  cta.classList.add("about-cta-wrapper");
 }
